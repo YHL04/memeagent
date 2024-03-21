fixed per numerical precision by casting to int

__plot__.py
@@ -10,7 +10,7 @@ import seaborn as sns
 
 dir = os.listdir("logs")
 dir.sort()
-latestfile = "logs/" + dir[-1]
+latestfile = "logs/" + dir[-2]
 
 data = pd.read_csv(latestfile,
                    names=["Time",
@@ -24,9 +24,9 @@ data = pd.read_csv(latestfile,
                           "Beta"]
                    )
 
-# data["Temp"] = np.arange(len(data["Reward"])) // 50
-# sns.lineplot(data=data, x="Temp", y="Reward")
-# plt.show()
+data["Minutes"] = np.arange(len(data["Reward"])) // 50
+sns.lineplot(data=data, x="Minutes", y="Reward")
+plt.show()
 
 plt.subplot(7, 1, 1)
 plt.title("Time (Sec)")

agent/per.py
@@ -15,22 +15,30 @@ class SumTree:
     for global Sum Tree to keep track of total priorities and retrieval over
     the entire buffer.
 
-    Need to review for bugs...
-    """
-    e = 0.01
+    NOTE:
+        AssertionError: 133.14080000000007 66.5704 66.5704
+        during _check_graph() caused by numerical precision
+        this causes issues occasionally when errors accumulate
+        the workaround is to use np.int64 array and multiply
+        float by a million and round it off so there is no numerical
+        precision errors that can cause a crash
 
-    def __init__(self, size, max_error, fill_zero=False):
+    """
+
+    def __init__(self, size, fill_value=0):
         """make sure size is powers of two and pad extra spots, not an optimal solution"""
+        fill_value = round(fill_value * 1000000)
+
         orig_size = size
         size = 2 ** math.ceil(math.log(size, 2))
 
         self.size = size
         self.orig_size = orig_size
-        self.tree = np.zeros((2 * size - 1,), dtype=np.float32)
+        self.tree = np.zeros((2 * size - 1,), dtype=np.int64)
 
-        if not fill_zero:
+        if fill_value != 0:
             for t in range(orig_size):
-                self.add(t, max_error, got_p=False)
+                self.update(t, fill_value)
 
         # self._check_graph()
 
@@ -44,36 +52,17 @@ class SumTree:
             # make sure that parent is the sum of left and right
             # and answer is within 1e-6 to account for np.float32 precision when comparing
             error = str(self.tree[parent]) + " " + str(self.tree[left]) + " " + str(self.tree[right])
-            assert 1e-3 > (self.tree[parent] - (self.tree[left] + self.tree[right])), error
+            assert 0 == (self.tree[parent] - (self.tree[left] + self.tree[right])), error
 
     def total(self):
-        return self.tree[0]
+        return self.tree[0] / 1000000
 
-    def get_priority(self, e):
-        # still unsure why sometimes e is nan
-        if np.isnan(e):
-            e = 0
-
-        # proportional prioritization
-        p = np.abs(e) + self.e
-
-        return p
-
-    def add(self, idx, p, got_p):
-        self.update(idx, p, got_p=got_p)
-
-        # self._check_graph()
-
-    def update(self, idx, e, got_p):
+    def update(self, data_idx, p):
         """changes tree[idx] to p and then propagate change through the tree"""
-        # convert data idx to tree idx
-        idx = idx + self.size - 1
+        p = round(p * 1000000)
 
-        # get priority from equation
-        if not got_p:
-            p = self.get_priority(e)
-        else:
-            p = e
+        # convert data idx to tree idx
+        idx = data_idx + self.size - 1
 
         change = p - self.tree[idx]
 
@@ -91,17 +80,17 @@ class SumTree:
 
     def get(self, s):
         """get index of data and its priority"""
-        # log = str(s) + " " + str(self.total())
-        # assert s <= self.total(), log
+        s = round(s * 1000000)
 
+        assert s != 0
+        assert s < self.tree[0]
         idx, s = self.retrieve(0, s)
-
         data_idx = idx - self.size + 1
 
-        # assert data_idx < self.orig_size
-        # assert s <= self.tree[idx]
-
-        return data_idx, self.tree[idx], s
+        assert self.tree[idx] != 0
+        p = self.tree[idx] / 1000000
+        s = s / 1000000
+        return data_idx, p, s
 
     def retrieve(self, idx, s):
         """when retrieve -1 is padding for sum tree if episode timesteps are not powers of two"""
@@ -111,11 +100,34 @@ class SumTree:
         if left >= len(self.tree):
             return idx, s
 
-        # temporary fix for a bug that happens infrequently ( or self.tree[right] == 0: )
-        # BUG: buffer sum tree sometimes retrieve a leaf node with priority 0
-        # best explanation is due to numerical precision of np.float32, occasionally
-        # messes up comparison of s and node (Example: 6.0600004 = 4.04 + 2.02)
         if s <= self.tree[left] or self.tree[right] == 0:
             return self.retrieve(left, s)
         else:
             return self.retrieve(right, s - self.tree[left])
+
+
+if __name__ == "__main__":
+    b = SumTree(size=16, fill_value=8.3223)
+    print(b._check_graph())
+    print(b.tree)
+
+    b = SumTree(size=16, fill_value=8.3212)
+    print(b._check_graph())
+    print(b.tree)
+
+    b = SumTree(size=16, fill_value=8.7632)
+    print(b._check_graph())
+    print(b.tree)
+
+    b = SumTree(size=16, fill_value=8.1122)
+    print(b._check_graph())
+    print(b.tree)
+
+    b = SumTree(size=16, fill_value=8.5123)
+    print(b._check_graph())
+    print(b.tree)
+
+    b = SumTree(size=16, fill_value=8.1842)
+    print(b._check_graph())
+    print(b.tree)
+

agent/replaybuffer.py
@@ -67,15 +67,17 @@ class ReplayBuffer:
         priority_queue (mp.Queue): FIFO queue to update new recurrent states from training to ReplayBuffer
 
     """
+    e = 0.01
     p_a = 0.6
     p_beta = 0.4
     p_beta_increment_per_sampling = 0.001
 
-    def __init__(self, size, B, T, N,
+    def __init__(self, max_frames, B, T, N,
                  sample_queue, batch_queue, priority_queue
                  ):
 
-        self.size = size
+        self.max_frames = int(max_frames)
+        self.size = int(max_frames)
         self.B = B
         self.T = T
         self.N = N
@@ -86,18 +88,22 @@ class ReplayBuffer:
         self.priority_queue = priority_queue
 
         # Buffer
-        self.buffer = np.empty((size,), dtype=object)
+        self.buffer = np.empty((self.size,), dtype=object)
         self.ptr, self.count, self.n_entries, self.updates = 0, 0, 0, 0
 
         # Global Sum Tree
-        self.sumtree = SumTree(size, max_error=0, fill_zero=True)
+        self.sumtree = SumTree(self.size)
 
         # Logger
         self.logger = Logger()
 
         self.frames = 0
+        self.total_frames = 0
         self.max_error = 1
         self.total_p = 0
+
+    def get_priority(self, e):
+        return np.abs(e) + self.e
 
     def __len__(self):
         return len(self.buffer)
@@ -158,17 +164,41 @@ class ReplayBuffer:
         with self.lock:
 
             # add to buffer
+
+            # total frames received
             self.frames += episode.length
+
+            # total frames in buffer
+            self.total_frames += episode.length
+
+            # remove episodes until total_frames can fit into max_frames
+            while self.total_frames > self.max_frames:
+                if self.buffer[-1] is not None:
+                    self.total_frames -= self.buffer[-1].length
+                    self.n_entries -= (self.buffer[-1].length - self.T)
+                    self.count -= 1
+
+                self.size -= 1
+                self.sumtree.update(self.size, 0)
+                self.buffer.resize(self.size, refcheck=False)
+                assert len(self.buffer) == self.size
+
+            if self.ptr >= self.size:
+                self.ptr = 0
+
+            # assert sum(self.sumtree.tree[len(self.buffer)+self.sumtree.size-1:]) == 0
+
             # create sum tree for priority queue
-            episode.sumtree = SumTree(episode.length-self.T, self.max_error, fill_zero=False)
+            episode.sumtree = SumTree(episode.length-self.T, fill_value=self.get_priority(self.max_error))
             # subtract entries that will be removed along with episode
             if self.buffer[self.ptr] is not None:
                 self.n_entries -= episode.length-self.T
+                self.total_frames -= episode.length
             # append episode to buffer
             self.buffer[self.ptr] = episode
             # update global sumtree
             assert episode.sumtree.total() != 0
-            self.sumtree.add(self.ptr, episode.sumtree.total(), got_p=True)
+            self.sumtree.update(self.ptr, episode.sumtree.total())
 
             # increment pointers and counts
             self.n_entries += episode.length-self.T
@@ -181,7 +211,7 @@ class ReplayBuffer:
             # logs
             self.logger.total_frames += episode.length
             self.logger.arm = episode.arm
-            self.logger.replay_ratio = (self.updates * self.B * self.T) / self.n_entries
+            self.logger.replay_ratio = (self.updates * self.B * self.T) / self.frames
 
             # obtain extrinsic reward from purely exploitative policy
             if episode.arm == 0:
@@ -229,8 +259,11 @@ class ReplayBuffer:
                 s = random.uniform(a, b)
 
                 b_idx, p, s = self.sumtree.get(s)
+                assert b_idx < self.size
                 t_idx, p, _ = self.buffer[b_idx].sumtree.get(s)
+                assert t_idx < self.buffer[b_idx].length - self.T
 
+                assert p >= 0
                 priorities.append(p)
                 idxs.append([b_idx, t_idx, self.buffer[b_idx].signature])
 
@@ -267,8 +300,14 @@ class ReplayBuffer:
             assert torch.isnan(torch.tensor(priorities)).any() == False, priorities
 
             priorities = torch.tensor(priorities) ** self.p_a
+            assert torch.isnan(priorities).any() == False, priorities
             sampling_probabilities = priorities / priorities.sum()
+            assert priorities.sum() != 0
+            assert torch.isnan(sampling_probabilities).any() == False, sampling_probabilities
             is_weights = torch.pow(self.n_entries * sampling_probabilities, -self.p_beta)
+            assert is_weights.max() != 0
+            assert torch.isnan(is_weights).any() == False, is_weights
+
             is_weights /= is_weights.max()
             assert torch.isnan(is_weights).any() == False
 
@@ -313,11 +352,16 @@ class ReplayBuffer:
                 # b_idx is the index of episode and t_idx is the starting index of burnin
                 b_idx, t_idx, signature = idx
 
+                if b_idx >= self.size:
+                    continue
+
                 if signature != self.buffer[b_idx].signature:
                     continue
 
-                self.buffer[b_idx].sumtree.update(t_idx, error, got_p=False)
-                self.sumtree.update(b_idx, self.buffer[b_idx].sumtree.total(), got_p=True)
+                assert np.abs(error) != np.nan, loss
+
+                self.buffer[b_idx].sumtree.update(t_idx, self.get_priority(error))
+                self.sumtree.update(b_idx, self.buffer[b_idx].sumtree.total())
 
                 if error > self.max_error and np.abs(error) != np.nan:
                     self.max_error = error

utils/soft_watkins.py
@@ -121,6 +121,17 @@ def compute_loss(q_t, qT_t, a_t, a_t1, r_t, pi_t1, mu_t1,
     assert arms.shape == (B,)
     assert is_weights.shape == (B,)
 
+    assert not torch.isnan(q_t).any()
+    assert not torch.isnan(qT_t).any()
+    assert not torch.isnan(a_t).any()
+    assert not torch.isnan(a_t1).any()
+    assert not torch.isnan(r_t).any()
+    assert not torch.isnan(pi_t1).any()
+    assert not torch.isnan(mu_t1).any()
+    assert not torch.isnan(discount_t).any()
+    assert not torch.isnan(arms).any()
+    assert not torch.isnan(is_weights).any()
+
     with torch.no_grad():
         # compute cutting trace coefficients in retrace
         # from what I understand: c_t1 is a way to correct for off policy samples
@@ -131,20 +142,24 @@ def compute_loss(q_t, qT_t, a_t, a_t1, r_t, pi_t1, mu_t1,
 
         # soft watkins Q(lambda): except that not all values are expected value
         q_a_t1 = get_index(q_t[1:], a_t1)
+        assert not torch.isnan(q_a_t1).any()
         indicator = (q_a_t1.unsqueeze(-1) >= q_t[1:] - kappa * torch.abs(q_t[1:])).float()
+        assert not torch.isnan(indicator).any()
         c_t1 = lambda_ * (pi_t1 * indicator).sum(-1)
+        assert not torch.isnan(c_t1).any()
 
         # get transformed targets
         target = compute_soft_watkins_target(q_t[1:], a_t1, r_t, discount_t, c_t1, pi_t1)
+        assert not torch.isnan(target).any()
 
     # get expected q value of taking action a_t
     expected = get_index(q_t[:-1], a_t)
     expectedT = get_index(qT_t, a_t)
 
-    assert torch.isnan(expected).any() == False, expected
-    assert torch.isnan(target).any() == False, target
-
     td_error = target - expected
+    assert not torch.isnan(expected).any()
+    assert not torch.isnan(target).any()
+    assert not torch.isnan(td_error).any()
 
     # trust region mask (A1)
     with torch.no_grad():
@@ -152,9 +167,11 @@ def compute_loss(q_t, qT_t, a_t, a_t1, r_t, pi_t1, mu_t1,
         # according to 𝜎 = max(𝜎running, 𝜎batch, 𝜖), assuming batch is std of current td_errors
         # sigma = max(running_error.std(), td_error.std().item(), 0.01)
         running_stds = torch.tensor([x.std() for x in running_errors], device=diff.device)
+        assert not torch.isnan(running_stds).any()
         batch_stds = td_error.view(-1, N).std(dim=0)
-
+        assert not torch.isnan(batch_stds).any()
         sigma = torch.maximum(torch.maximum(running_stds, batch_stds), torch.tensor(0.01))
+        assert not torch.isnan(sigma).any()
         mask = (torch.abs(diff) > alpha * sigma.view(1, 1, N).repeat(T, B, 1)) & (torch.sign(diff) != expected - target)
 
     # update 𝜎running
@@ -163,15 +180,22 @@ def compute_loss(q_t, qT_t, a_t, a_t1, r_t, pi_t1, mu_t1,
 
     # loss and priority normalization (B1)
     # (T, B, N) / (N,)
+    assert not torch.isnan(td_error).any()
     td_error = td_error / sigma
+    assert not torch.isnan(td_error).any()
 
     loss = 0.5 * (td_error ** 2)
+    assert not torch.isnan(loss).any()
     loss = torch.where(mask, 0., loss)
+    assert not torch.isnan(loss).any()
 
     # Cross mixture loss (B2)
+    assert not torch.isnan(loss).any()
     loss = n * get_index(loss, arms.unsqueeze(0).repeat(T, 1)) + ((1-n) / N) * loss.sum(-1)
+    assert not torch.isnan(loss).any()
     loss *= is_weights
     loss = loss.mean()
+    assert not torch.isnan(loss).any()
 
     return loss, td_error.sum(-1).detach()
 